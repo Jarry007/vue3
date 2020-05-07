e30b7c7b3eb6e469f9525266e43fe73f3ed23bfc
Jarry007 <940615834@qq.com>
2020-05-07 18:24:51 +0800

    :tada: 尝试一下vue3中router的用法

---
+++ src/views/About.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="about">
+    <h1 @click="addClick">This is an about page{{double}}</h1>
+    <h2 v-for="i in count" :key='i' >{{i}}</h2>
+  </div>
+</template>
+
+<script>
+import {reactive,toRefs, watch, computed} from 'vue'
+export default  {
+  name:'about',
+  setup(){
+    const data = reactive({
+      obj:[1,2,3,4,5,6,7,8,9],
+      count:6,
+      double:computed(()=>data.count * 2,)
+    })
+    const addClick = ()=>{
+      data.count ++ 
+    }
+    watch(()=>data.count,val=>{
+      console.log('va',val)
+    })
+    return {...toRefs(data),addClick}
+  }
+}
+</script>
